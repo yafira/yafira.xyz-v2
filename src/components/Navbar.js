38e1e0b3ce6cc1c62d03f50d461602b2951461ed
Yafira <yafira.martinez@lc.cuny.edu>
2022-03-25 17:31:56 -0400

    added alt prop to dark/light icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,11 +6,11 @@ export default function Navbar(props) {
 	return (
 		<nav className={props.darkMode ? 'dark' : ''}>
 			<div className='toggler'>
-				<img className='toggler--light' src={lightIcon} />
+				<img className='toggler--light' src={lightIcon} alt='light-mode' />
 				<div className='toggler--slider' onClick={props.toggleDarkMode}>
 					<div className='toggler--slider--circle'></div>
 				</div>
-				<img className='toggler--dark' src={darkIcon} />
+				<img className='toggler--dark' src={darkIcon} alt='dark-mode' />
 			</div>
 		</nav>
 	)
