e4b65c946f437fd73fcacc4c5c9eb9ca10f01f20
Yafira <yafira.martinez@lc.cuny.edu>
2022-03-25 15:04:14 -0400

    replaced dark/light mode text with icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import lightIcon from '../assets/light.png'
+import darkIcon from '../assets/dark.png'
 
 export default function Navbar(props) {
 	return (
 		<nav className={props.darkMode ? 'dark' : ''}>
 			<div className='toggler'>
-				<p className='toggler--light'>Light</p>
+				<img className='toggler--light' src={lightIcon} />
 				<div className='toggler--slider' onClick={props.toggleDarkMode}>
 					<div className='toggler--slider--circle'></div>
 				</div>
-				<p className='toggler--dark'>Dark</p>
+				<img className='toggler--dark' src={darkIcon} />
 			</div>
 		</nav>
 	)
