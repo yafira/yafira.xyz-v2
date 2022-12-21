cd430bb2751ec10d77cf033ecc546314f51d79b1
Yafira <yafira.martinez@lc.cuny.edu>
2022-12-20 19:54:41 -0500

    minor grammar edit

--- src/components/Main.js
+++ src/components/Main.js
@@ -66,10 +66,10 @@ export default function Main(props) {
 					</a>
 					, ♡ loves having <span className='tea'>tea </span> and&nbsp;
 					<span className='confections'>confections </span> (like
-					<span className='wagashi'> wagashi</span>), game on her&nbsp;
-					<span className='nintendo'>nintendo</span>,&nbsp;
-					<span className='checker'>play chess</span>, be in&nbsp;
-					<span className='nature'>nature</span>,&nbsp;
+					<span className='wagashi'> wagashi</span>),&nbsp;
+					<span className='nintendo'>game on her nintendo</span>,&nbsp;
+					<span className='checker'>play chess</span>,&nbsp;
+					<span className='nature'>be in nature</span>,&nbsp;
 					<span className='travel'>explore new locations ✶ </span> and always
 					learn new things.
 				</p>
@@ -136,9 +136,9 @@ export default function Main(props) {
 						target='_blank'
 						rel='noopener noreferrer'
 					>
-						Are.na
+						Are.na&nbsp;
 					</a>
-					&nbsp;& you can also reach her via&nbsp;
+					& you can also reach her via&nbsp;
 					<a
 						id='email'
 						href='mailto: yfr.mrtnz@gmail.com'
